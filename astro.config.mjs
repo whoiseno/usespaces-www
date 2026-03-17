88e9cbbf31adcb367479e3ca87c2bb90d1df545f
eno? <enoabasidream@gmail.com>
2026-03-17 14:43:51 +0100

    feat: setup tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
 import netlify from "@astrojs/netlify";
+import tailwindcss from "@tailwindcss/vite";
 // @ts-check
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: netlify(),
+  adapter: netlify({
+    devFeatures: {
+      images: false,
+      environmentVariables: false,
+    },
+  }),
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
